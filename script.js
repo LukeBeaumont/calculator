3423747771436d99c9b8bb0d7c3afd4195c087ef
LukeBeaumont <lbeaumont50@msn.com>
2023-02-20 19:09:20 +0000

    Link elements in javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentValue = "";
+let previousValue = "";
+let operator = "";
+
+const currentDisplay = document.querySelector(".current");
+const previousDisplay = document.querySelector(".previous");
+
+const clear = document.querySelector("#clear");
+const decimal = document.querySelector("#decimal");
+const equals = document.querySelector("#equals");
+
+const numbers = document.querySelector(".number");
+const operators = document.querySelector(".operator");
+
