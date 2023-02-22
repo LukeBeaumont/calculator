4803acca124d3f2f5cdd42134f1a3fffc6a12a13
LukeBeaumont <lbeaumont50@msn.com>
2023-02-22 21:14:16 +0000

    Make initial calculate function

--- script.js
+++ script.js
@@ -5,17 +5,29 @@ let currentOperator = "";
 const currentDisplay = document.querySelector(".current");
 const previousDisplay = document.querySelector(".previous");
 
-const clear = document.querySelector("#clear");
+const clear = document.querySelector(".clear");
 const decimal = document.querySelector("#decimal");
-const equals = document.querySelector("#equals");
+const equals = document.querySelector(".equals");
 
 let numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
+
+clear.addEventListener("click", clearCalc);
+
+equals.addEventListener("click", calculate(currentValue,previousValue));
 
 numbers.forEach((num)=> num.addEventListener("click", (e)=> handleNumber(e.target.textContent)));
 
 operators.forEach((op) => op.addEventListener("click", (e)=> handleOperator(e.target.textContent)))
  
+function clearCalc() {
+    currentValue = "";
+    previousValue = "";
+    currentOperator = "";
+    currentDisplay.textContent = "";
+    previousDisplay.textContent = "";
+}
+
 function handleNumber(num) {
     if(currentValue.length <= 9) {
         currentValue += num;
@@ -25,7 +37,14 @@ function handleNumber(num) {
 
 function handleOperator(op) {
     currentOperator = op;
-    currentDisplay.textContent = currentOperator;
-    previousDisplay.textContent = currentValue;
+    previousValue = currentValue + " " + currentOperator;
+    previousDisplay.textContent = previousValue;
+    currentDisplay.textContent = "";
     currentValue = "";
+}
+
+function calculate(firstNumber,secondNumber) {
+    if(currentOperator === "+") {
+        currentDisplay.textContent = firstNumber + secondNumber;
+    }
 }
